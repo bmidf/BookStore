d3f5af0cf353fa129e6864376f02b16d33cb2de5
Beka Makharadze <81828475+bmidf@users.noreply.github.com>
2022-05-10 23:34:30 +0400

    Update CustomDrawer.js

--- src/components/CustomDrawer.js
+++ src/components/CustomDrawer.js
@@ -1,21 +1,33 @@
 import React from "react";
-import {View, Text, StyleSheet} from "react-native";
+import {View, Text, StyleSheet,onPress} from "react-native";
 import {Avatar} from "react-native-paper";
-import {DrawerContentScrollView,DrawerItemList,} from "@react-navigation/drawer";
+import {DrawerContentScrollView,DrawerItem,DrawerItemList} from "@react-navigation/drawer";
 import {TouchableOpacity} from "react-native-gesture-handler";
+import {useNavigation } from '@react-navigation/core';
+import {removeUser} from '../service/UserService'
 
 export const CustomDrawer = (props) => {
+  let navigation = useNavigation();
   return (
     <View style={{flex: 1}}>
-      <DrawerContentScrollView {...props}>
+      <DrawerContentScrollView {...props} >
         <View style={{flex: 1}}>
-          <TouchableOpacity style={styles.profile}> 
+          <TouchableOpacity style={styles.profile} onPress={()=> navigation.navigate('Profile')}> 
             <Avatar.Image source={{uri: "https://vivek.pyther.com/books/jane-profile.jpg"}}/>
             <Text style={styles.name}>Jane Doe</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.edit} onPress={()=> navigation.navigate('Profile')}> 
+          <Text style={styles.editText}>Edit Profile</Text>
           </TouchableOpacity>
           <View style={styles.line}></View>
         </View>
         <DrawerItemList {...props} />
+        <DrawerItem
+          label="LOG OUT"
+          onPress={() => {
+            navigation.navigate('GetStarted');
+              removeUser();
+            }}/>
       </DrawerContentScrollView>
     </View>
   );
@@ -33,6 +45,18 @@ const styles = StyleSheet.create({
     margin: 15,
     color: "#4e6882",
     fontFamily: 'Montserrat_700Bold'
+  },
+  edit: {
+    backgroundColor: '#d9d9d9',
+    width: 100,
+    borderRadius: 100,
+    padding: 10,
+    marginLeft: 30,
+    marginBottom: 10,
+  },
+  editText: {
+    fontFamily: 'Montserrat_500Medium',
+    textAlign: 'center',
   },
   line:{
     marginLeft: 10,
