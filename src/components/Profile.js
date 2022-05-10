9b00f2670c08ed7fa877b81bc1a7c3cbc8bab888
Beka Makharadze <81828475+bmidf@users.noreply.github.com>
2022-05-10 23:33:04 +0400

    Create Profile.js

---
+++ src/components/Profile.js
@@ -0,0 +1,7 @@
+import {Text} from 'react-native';
+
+export default function Profile(){
+    return(
+        <Text style={{textAlign: 'center',fontSize: 18,marginTop: 250}}>Profile</Text>
+    )
+}
